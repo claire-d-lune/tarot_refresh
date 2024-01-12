10358fd2a438eb287cc9434a39ea7e22f54c33eb
Claire Steinhoff <claire.steinhoff@gmail.com>
2024-01-12 12:12:27 -0700

    Updating branch before working on filters.

--- frontend/src/components/CardList.tsx
+++ frontend/src/components/CardList.tsx
@@ -3,8 +3,6 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import TarotCard from "./TarotCard.tsx"
 
-// import { imageObject } from '../assets/Cups/ImageCollection'
-
 const CardList = () => {
 
     type Card = {
@@ -22,32 +20,40 @@ const CardList = () => {
     
     const [cardList, setCardList] = useState<Card[] |null>(null);
     
-    // let response: Card[]
+    // Fetch card data into use state as a custom data type
     useEffect(() => {
         axios('/api/cards')
         .then(res => setCardList([...res.data]))
     }, []);
 
     console.log(cardList)
+
+    // Building card filter. 
+    const [filterSettings, setFilterSettings] = useState()
+    
+    const handleCategory = (event: React.MouseEvent) => {
+        console.log(event)
+    }
   
     return(
         <div>
             <Navbar/>
-            <p> Hello From the Cardlist! </p>
+            <div className="dropdown">
+                <label tabIndex={0} className="btn m-1">Filter</label>
+                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                    <li onClick={handleCategory}>Swords</li>
+                    <li onClick={handleCategory}>Pentacles</li>
+                    <li onClick={handleCategory}>Cups</li>
+                    <li onClick={handleCategory}>Wands</li>
+                </ul>
+            </div>
             <div>
                 {cardList ? cardList.map(cardDetails => <TarotCard {...cardDetails}/>):  null}
             </div>
         </div>
         )
-
-    // Building card filter. 
-    const [filterSettings, setFilterSettings] = useState(["","any"])
-    
-    const handleCategory = (e) => {
-        console.log(e)
-    }
-    
-
+    // Indicate a selected card to show extra data for one card at a time. 
+    // const [selectedCard, setSelectedCard] = useState(id: Number)
 }
 
 export default CardList
